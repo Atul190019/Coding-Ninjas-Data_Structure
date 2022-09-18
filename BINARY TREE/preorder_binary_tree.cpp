c64c11055a8dfda6f83b03419c241a2df6229889
Atul190019 <106909363+Atul190019@users.noreply.github.com>
2022-09-18 23:48:59 +0530

    Create preorder_binary_tree.cpp

---
+++ BINARY TREE/preorder_binary_tree.cpp
@@ -0,0 +1,8 @@
+void preOrder(BinaryTreeNode<int> *root) {
+// Write your code here
+    if(root==NULL)
+        return ;
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+}

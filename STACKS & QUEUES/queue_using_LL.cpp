24b74726f1ddb8fd1832b5534239a576096cbde1
Atul190019 <106909363+Atul190019@users.noreply.github.com>
2022-09-19 00:34:47 +0530

    Create queue_using_LL.cpp

---
+++ STACKS & QUEUES/queue_using_LL.cpp
@@ -0,0 +1,29 @@
+class Queue {
+	// Define the data members
+   public:
+    Queue() {
+		// Implement the Constructor
+	}
+	
+	/*----------------- Public Functions of Stack -----------------*/
+
+	int getSize() {
+		// Implement the getSize() function
+	}
+
+    bool isEmpty() {
+		// Implement the isEmpty() function
+	}
+
+    void enqueue(int data) {
+		// Implement the enqueue() function
+	}
+
+    int dequeue() {
+        // Implement the dequeue() function
+    }
+
+    int front() {
+        // Implement the front() function
+    }
+};

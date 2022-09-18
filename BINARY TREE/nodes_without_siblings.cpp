6aa0372e5c817e2f6ece0feb7c146331e9f075c9
Atul190019 <106909363+Atul190019@users.noreply.github.com>
2022-09-19 00:01:25 +0530

    Create nodes_without_siblings.cpp

---
+++ BINARY TREE/nodes_without_siblings.cpp
@@ -0,0 +1,15 @@
+void printNodesWithoutSibling(BinaryTreeNode<int> *root) {
+    // Write your code here
+    if(root==NULL)
+        return ;
+    
+    if(root->left!=NULL && root->right==NULL)
+        cout<<root->left->data<<" ";
+    if(root->left==NULL && root->right!=NULL)
+        cout<<root->right->data<<" ";
+        
+    printNodesWithoutSibling(root->left);
+    printNodesWithoutSibling(root->right);
+    
+    
+}

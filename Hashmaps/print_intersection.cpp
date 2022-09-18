4e7991dd8c14ecfdf37da0880e3a0198f1e5075a
Atul190019 <106909363+Atul190019@users.noreply.github.com>
2022-09-18 23:06:56 +0530

    Create print_intersection.cpp

---
+++ Hashmaps/print_intersection.cpp
@@ -0,0 +1,33 @@
+#include <unordered_map>
+void printIntersection(int arr1[], int arr2[], int n, int m)
+{
+    // Write your code here
+    unordered_map<int, int> mp;
+    if (n > m)
+    {
+        for (int i = 0; i < m; i++)
+            mp[arr2[i]]++;
+        for (int i = 0; i < n; i++)
+        {
+            if (mp.count(arr1[i]) == 1 && mp[arr1[i]] > 0)
+            {
+                cout << arr1[i] << endl;
+                mp[arr1[i]]--;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+            mp[arr1[i]]++;
+        for (int i = 0; i < m; i++)
+        {
+            if (mp.count(arr2[i]) == 1 && mp[arr2[i]] > 0)
+            {
+                cout << arr2[i] << endl;
+                mp[arr2[i]]--;
+            }
+        }
+    }
+}
+

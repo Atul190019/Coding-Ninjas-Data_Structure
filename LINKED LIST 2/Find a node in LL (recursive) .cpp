46b119b4b977adbea31e16910b6caff38cd9a2a8
Atul190019 <106909363+Atul190019@users.noreply.github.com>
2022-09-19 00:55:43 +0530

    Create Find a node in LL (recursive) .cpp

---
+++ LINKED LIST 2/Find a node in LL (recursive) .cpp
@@ -0,0 +1,17 @@
+int findNodeRec(Node *head, int n) {
+    // Write your code here
+    if(head==NULL)
+        return -1;
+    if(head->data==n)
+        return 0;
+    
+    int x=findNodeRec(head->next,n);
+    if(x==-1)
+    {
+        return x;
+    }
+    else
+        return x+1;
+
+
+}

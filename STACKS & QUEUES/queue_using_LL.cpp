d5719edddea14d3261dcf8c8618daa7bb31e4895
Atul190019 <106909363+Atul190019@users.noreply.github.com>
2022-09-20 00:17:19 +0530

    Update queue_using_LL.cpp

--- STACKS & QUEUES/queue_using_LL.cpp
+++ STACKS & QUEUES/queue_using_LL.cpp
@@ -1,29 +1,65 @@
 class Queue {
 	// Define the data members
+    Node *head;
+    Node *tail;
+    int size;
+    
    public:
     Queue() {
 		// Implement the Constructor
+        head = NULL;
+        tail = NULL;
+        size = 0;
 	}
 	
 	/*----------------- Public Functions of Stack -----------------*/
 
 	int getSize() {
 		// Implement the getSize() function
+        return size;
 	}
 
     bool isEmpty() {
 		// Implement the isEmpty() function
+        return size == 0;
 	}
 
     void enqueue(int data) {
 		// Implement the enqueue() function
+        size++;
+        Node *newNode = new Node(data);
+        
+        if (head == NULL){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
 	}
 
     int dequeue() {
         // Implement the dequeue() function
+        if (isEmpty()) {
+            return -1;
+        }
+        
+        int ans = head->data;
+        
+        Node *tmp = head;
+        head = head->next;
+        delete tmp;
+        
+        size--;
+        
+        return ans;
     }
 
     int front() {
         // Implement the front() function
+        if (isEmpty()) {
+            return -1;
+        }
+        return head->data;
     }
 };

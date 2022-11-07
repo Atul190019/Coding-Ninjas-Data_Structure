8a41f651286c17d54728692fae77c0d87254bc69
Atul190019 <106909363+Atul190019@users.noreply.github.com>
2022-11-07 07:47:33 +0530

    Update remove pi(recursive).cpp

--- Recursion 1b/remove pi(recursive).cpp
+++ Recursion 1b/remove pi(recursive).cpp
@@ -28,7 +28,7 @@ void replacePi (char input[])
     replacePi (input, 0);
 }*/
 
-#include <cstring>
+/*#include <cstring>
 void replacePi(char input[])
 {
     // Write your code here
@@ -47,4 +47,22 @@ void replacePi(char input[])
         replacePi(input + 1);
     else
         return;
+}*/
+
+#include <cstring>
+void replacePi(char input[]) {
+  // Write your code here
+  int l = strlen(input);
+  if (input[0] == 'p' && input[1] == 'i') {
+    for (int j = l; j > 1; j--)
+      input[j + 2] = input[j];
+    input[0] = '3';
+    input[1] = '.';
+    input[2] = '1';
+    input[3] = '4';
+    replacePi(input + 4);
+  } else if (input[1] != '\0')
+    replacePi(input + 1);
+  else
+    return;
 }
